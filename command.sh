247f9c39e219e29cddadef0c907c982bf8592fee
attalg <attalg@gmail.com>
2018-06-03 00:14:12 +1000

    trial K8s deployment

--- command.sh
+++ command.sh
@@ -2,7 +2,12 @@
 
 make_cmd=$1
 gcloud_project= $2
-gcloud_sa_key_path=$3
+gcloud_sa_key=$3
+gcloud_sa_key_path=key.json
+
+cat > key.json <<EOF
+$gcloud_sa_key
+EOF
 
 gcloud gcloud auth activate-service-account test-service-account@google.com --key-file=$gcloud_sa_key_path --project=$gcloud_project
 
